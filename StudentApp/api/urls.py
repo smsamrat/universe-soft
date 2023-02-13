5569b82e5690ae8a7092513ebc7d0f291331db21
S.M.Samrat <97931972+smsamrat@users.noreply.github.com>
2023-02-13 18:29:43 +0600

    user and student profile update done

--- StudentApp/api/urls.py
+++ StudentApp/api/urls.py
@@ -3,12 +3,13 @@ from StudentApp.api.views import *
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('courses', CourseSerializerView, basename='course_id')
+# router.register('courses', CourseSerializerView, basename='course_id'),
+router.register('student_pro', StudentProfileView, basename='student_pro')
 
 urlpatterns = [
-    path('courses_view/',include(router.urls)),
+    path('f/',include(router.urls)),
     path('register/',UserRegistrationView.as_view(), name='register'),
-    path('cdetail/',StudentSerializersView.as_view(), name='course-detail'),
+    path('register/<int:id>/',UserRegistrationView.as_view(), name='register'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
 ]
